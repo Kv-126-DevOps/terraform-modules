fe7b5cd7a3d5d7cf141b041538aae8b2f11d6ce4
AlexPokatilov <sasha.pokatilov@gmail.com>
2022-06-27 02:37:14 +0300

    undo

--- rabbit-mq-module/outputs.tf
+++ rabbit-mq-module/outputs.tf
@@ -1,49 +1,49 @@
 output "id" {
   description = "The ID of the broker"
-  value       = aws_mq_broker.rabbit[0].id
+  value       = aws_mq_broker.rabbit.id
 }
 
 output "arn" {
   description = "ARN of broker"
-  value       = aws_mq_broker.rabbit[0].arn
+  value       = aws_mq_broker.rabbit.arn
 }
 
 output "tags" {
   description = "Map of tags assigned to resources"
-  value       = aws_mq_broker.rabbit[0].tags
+  value       = aws_mq_broker.rabbit.tags
 }
 
 output "broker_name" {
   description = "Name to be used on broker created"
-  value       = aws_mq_broker.rabbit[0].broker_name
+  value       = aws_mq_broker.rabbit.broker_name
 }
 
 output "engine_type" {
   description = "Type of broker engine"
-  value       = aws_mq_broker.rabbit[0].engine_type
+  value       = aws_mq_broker.rabbit.engine_type
 }
 
 output "engine_version" {
   description = "Version of the broker engine"
-  value       = aws_mq_broker.rabbit[0].engine_version
+  value       = aws_mq_broker.rabbit.engine_version
 }
 
 output "host_instance_type" {
   description = "Broker's instance type"
-  value       = aws_mq_broker.rabbit[0].host_instance_type
+  value       = aws_mq_broker.rabbit.host_instance_type
 }
 
 output "security_groups" {
   description = "List of security group IDs assigned to the broker"
-  value       = aws_mq_broker.rabbit[0].security_groups
+  value       = aws_mq_broker.rabbit.security_groups
 }
 
 output "subnet_ids" {
   description = "List of subnet IDs in which to launch the broker"
-  value       = aws_mq_broker.rabbit[0].subnet_ids
+  value       = aws_mq_broker.rabbit.subnet_ids
 }
 
 output "endpoint" {
   description = "Broker's wire-level protocol endpoint"
-  value       = aws_mq_broker.rabbit[0].instances.0.endpoints
+  value       = aws_mq_broker.rabbit.instances.0.endpoints
 }
